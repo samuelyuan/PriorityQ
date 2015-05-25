692c57897e635637884d91837e91343fe49df05c
Chenrui26 <chenrui1993@gmail.com>
2015-05-25 14:26:30 -0400

    Modify the project

---
+++ Priority_Q/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Priority_Q.MvcApplication" Language="C#" %>
---
+++ Priority_Q/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Priority_Q.Startup))]
+namespace Priority_Q
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
