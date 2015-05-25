d809f6edcdd016bad124adab8349aa146ec97599
Samuel Yuan <samuelyuan@users.noreply.github.com>
2015-05-25 18:08:40 +0000

    Added basic skeleteon

---
+++ PriorityQ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PriorityQ.Startup))]
+namespace PriorityQ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
