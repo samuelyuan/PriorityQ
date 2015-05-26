ecb56ff0914e48950cf3d9c3f088040ff0d6d94e
Samuel Yuan <samuelyuan@users.noreply.github.com>
2015-05-26 20:04:01 +0000

    Added CRUD for restaurants
    The user can add new restaurants to the database and modify some basic
    information

---
+++ Priority_Q/Models/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Priority_Q.Models
+{
+    public class Restaurant
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int NumTables { get; set; }
+    }
+
+    public class RestaurantDBContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
