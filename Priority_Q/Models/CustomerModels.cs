ebc1435eacc0fe71d8f3048ae0f5ecd46425b739
Samuel Yuan <samuelyuan@users.noreply.github.com>
2015-06-02 00:47:30 +0000

    Add customers to a priority queue

---
+++ Priority_Q/Models/CustomerModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Priority_Q.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+
+        public int RestaurantID { get; set; } //customer goes to a specific restaurant
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The customer must have a name!")]
+        public string Name { get; set; }
+
+        [Required]
+        public int PhoneNumber { get; set; }
+    }
+
+    public class CustomerDBContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
