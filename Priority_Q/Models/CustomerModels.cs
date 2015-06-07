2b834f736680fafdd5acc1342f7a77de51e6f4ab
Samuel Yuan <samuelyuan@users.noreply.github.com>
2015-06-07 22:57:29 +0000

    More redesigning of site's layout
    Should be more compact than before

--- Priority_Q/Models/CustomerModels.cs
+++ Priority_Q/Models/CustomerModels.cs
@@ -15,6 +15,7 @@ namespace Priority_Q.Models
         public string Name { get; set; }
 
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Group capacity must be a positive number")]
         public int GroupCapacity { get; set; }
     }
 
