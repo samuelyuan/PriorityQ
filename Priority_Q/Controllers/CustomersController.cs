a9a12409b1154f7da1f84c417c8492233cad3b45
Samuel Yuan <samuelyuan@users.noreply.github.com>
2015-07-05 19:03:08 +0000

    Handling the priority queue is all done in a popup, instead of a separate page.

--- Priority_Q/Controllers/CustomersController.cs
+++ Priority_Q/Controllers/CustomersController.cs
@@ -15,49 +15,6 @@ namespace Priority_Q.Controllers
     {
         private CustomerDBContext db = new CustomerDBContext();
 
-        // GET: Customers
-        public ActionResult Index()
-        {
-            return RedirectToAction("Index", "Restaurants");
-        }
-
-        // GET: Customers/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Customer customer = db.Customers.Find(id);
-            if (customer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(customer);
-        }
-
-        // GET: Customers/Create/1
-        public ActionResult Create(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Customer customer = new Customer();
-            customer.RestaurantID = id.Value;
-
-            //user isn't logged in
-            if (!Request.IsAuthenticated)
-                return RedirectToAction("Index", "Restaurants");
-
-            //restaurant doesn't have this customer
-            Restaurant restaurant = (new RestaurantDBContext()).Restaurants.Find(customer.RestaurantID);
-            if (restaurant.UserID != User.Identity.GetUserId())
-                return RedirectToAction("Index", "Restaurants");
-
-            return View(customer);
-        }
-
         // POST: Customers/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -73,45 +30,6 @@ namespace Priority_Q.Controllers
             }
 
             return View(customer);
-        }
-
-        // GET: Customers/AssignTable/5
-        public ActionResult AssignTable(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            Customer customer = db.Customers.Find(id);
-            ViewBag.CustomerID = customer.ID;
-            ViewBag.CustomerName = customer.Name;
-            ViewBag.CustomerGroupSize = customer.GroupCapacity;
-
-            RestaurantDBContext RestaurantDB = new RestaurantDBContext();
-
-            //Restaurant restaurant = db.Restaurants.Find(id);
-            if (!Request.IsAuthenticated)
-                return RedirectToAction("Index", "Restaurants");
-
-            //restaurant doesn't have this customer
-            Restaurant restaurant = RestaurantDB.Restaurants.Find(customer.RestaurantID);
-            if (restaurant.UserID != User.Identity.GetUserId())
-                return RedirectToAction("Index", "Restaurants");
-
-            //Find all tables belonging to a restaurant 
-            TableDBContext tableDB = new TableDBContext();
-            IEnumerable<Priority_Q.Models.Table> tables = tableDB.Tables.Where(i => i.RestaurantId == restaurant.ID);
-            IEnumerable<Priority_Q.Models.Table> canSeatTables = tables.Where(i => i.MaxCapacity >= ViewBag.CustomerGroupSize); 
-            ViewBag.RestaurantId = restaurant.ID;
-            ViewBag.OwnsRestaurant = (restaurant.UserID == User.Identity.GetUserId());
-            ViewBag.RestaurantName = restaurant.Name;
-            ViewBag.RestaurantLocation = restaurant.City;
-
-            IEnumerable<Priority_Q.Models.Table> availableTables = canSeatTables.Where(table => table.IsOccupied == false);
-            ViewBag.AvailableTables = availableTables.Count();
-
-            return View(tables);
         }
 
         // GET: Customers/Edit/5
